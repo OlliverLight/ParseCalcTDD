8fb076bf95a154a3b3ce1b093e40a5eadca37fae
olliver <olliver@OLLIVERS-PC>
2021-01-29 19:32:51 +0100

    Add,Sub,mult working. Div by 0, unsure.

--- ParseCalcTDD/Program.cs
+++ ParseCalcTDD/Program.cs
@@ -6,38 +6,23 @@ namespace ParseCalcTDD
     {
         static void Main(string[] args)
         {
-            double Val1 = 0;
-            double Val2 = 0;
-
-            try
-            {
-                Console.Write("Enter your first value: ");
-                Val1 = double.Parse(Console.ReadLine());
-                Console.Write("Enter your second value: ");
-                Val2 = double.Parse(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("Something went wrong with your inputs. Please press any key to exit.");
-                Console.ReadKey();
-            }
-
-            double Add = Val1 + Val2;
-            double Sub = Val1 - Val2;
-            double Mul = Val1 * Val2;
-            double Div = Val1 / Val2;
-
-            Console.WriteLine(Val1 + " + " + Val2 + " = " + Add);
-            Console.WriteLine(Val1 + " - " + Val2 + " = " + Sub);
-            Console.WriteLine(Val1 + " * " + Val2 + " = " + Mul);
-            Console.WriteLine(Val1 + " / " + Val2 + " = " + Div);
 
         }
-        static public double Interperate(string Val1)
+        public static double Add(double x, double y)
         {
-
-            return 0; 
+            return x + y;
         }
-        
+        public static double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Mult(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Div(double x, double y)
+        {
+            return x / y;
+        }
     }
 }
--- ParseCalcTests/UnitTest1.cs
+++ ParseCalcTests/UnitTest1.cs
@@ -6,23 +6,49 @@ namespace ParseCalcTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void SimpleNumber()
+        [Theory]
+        [InlineData(5, 7, 12)]
+        [InlineData(20, 4.36, 24.36)]
+        [InlineData(double.MaxValue, 1, double.MaxValue)]
+        public void Addition_Simple_Numbers(double x, double y, double expected)
         {
-            Assert.Equal(6.0, Program.Interperate("6"));
+            double actual = Program.Add(x, y);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(9, 3, 6)]
+        [InlineData(37, 3.8, 33.2)]
+        [InlineData(double.MinValue, 5, double.MinValue)]
+        public void Subtraction_Simple_Numbers(double x, double y, double expected)
+        {
+            double actual = Program.Sub(x, y);
+            Assert.Equal(expected, actual);
+
+        }
+        [Theory]
+        [InlineData(3,5,15)]
+        [InlineData(3.5,23.6, 82.60000000000001)]
+        public void Multiply_Simple_Numbers(double x, double y, double expected)
+        {
+            double actual = Program.Mult(x, y);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(55, 27.5, 2 )]
+        public void Divide_Simple_Number(double x, double y, double expected)
+        {
+            double actual = Program.Div(x, y);
+            Assert.Equal(expected, actual);
 
         }
         [Fact]
-        public void Addition()
+        public void Devide_ByZero()
         {
-            Assert.Equal(6.0, Program.Interperate("3+3"));
-
+            double expected = 0;
+            double actual = Program.Div(20,0);
+            Assert.Equal(expected, actual);
         }
-        [Fact]
-        public void Subtraction()
-        {
-            Assert.Equal(6.0, Program.Interperate("9-3"));
 
-        }
     }
 }
